12f3bd1b6ef27281a2798685f8730bf346b48902
Benjamin <bjd.salaun@gmail.com>
2023-02-20 14:38:15 +0100

    third commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,39 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady);
 
 function onDeviceReady() {
-  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
-  document.getElementById("deviceready").classList.add("ready");
+  const formAddPizza = document.querySelector(".form-add-pizza");
+  const pizzasTag = document.querySelector(".pizzas");
+  const errorTag = document.querySelector(".error");
+
+  formAddPizza.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const pizzaName = formAddPizza.name.value;
+    const ingredients = formAddPizza.ingredients.value;
+
+    if (isValid(pizzaName) && isValid(ingredients)) {
+      const pizzaTag = document.createElement("div");
+      pizzaTag.className = "pizza";
+      pizzaTag.innerHTML = `
+        <h3>${capitalizeFirstLetter(pizzaName)}</h3>
+        <p>(${formatIngredients(ingredients)})</p>
+      `;
+
+      pizzasTag.insertBefore(pizzaTag, pizzasTag.firstElementChild);
+
+      formAddPizza.reset();
+    } else {
+      errorTag.innerHTML = "<p>Formulaire invalide !</p>";
+      setTimeout(() => (errorTag.innerHTML = ""), 2000);
+    }
+  });
+
+  function capitalizeFirstLetter(value) {
+    return value.trim().slice(0, 1).toUpperCase() + value.trim().slice(1);
+  }
+  function formatIngredients(value) {
+    return value.trim().replaceAll(" ", ", ");
+  }
+  function isValid(value) {
+    return value && value.length > 2 && value.length < 50;
+  }
 }
