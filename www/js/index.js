602b5bed0891abe980311a789a9c248f97711715
Benjamin <bjd.salaun@gmail.com>
2023-02-26 13:39:11 +0100

    TP Cordova Benjamin S

--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,28 @@
 document.addEventListener("deviceready", onDeviceReady);
 
 function onDeviceReady() {
-  const formAddPizza = document.querySelector(".form-add-pizza");
-  const pizzasTag = document.querySelector(".pizzas");
+  const formAddObject = document.querySelector(".form-add-object");
+  const objectsTag = document.querySelector(".objects");
   const errorTag = document.querySelector(".error");
 
-  formAddPizza.addEventListener("submit", (e) => {
+  formAddObject.addEventListener("submit", (e) => {
     e.preventDefault();
-    const pizzaName = formAddPizza.name.value;
-    const ingredients = formAddPizza.ingredients.value;
+    const objectName = formAddObject.name.value;
+    const objectDescription = formAddObject.description.value;
 
-    if (isValid(pizzaName) && isValid(ingredients)) {
-      const pizzaTag = document.createElement("ion-accordeon");
-      pizzaTag.className =
-        "pizza md accordion-animated hydrated accordion-collapsed";
-      pizzaTag.innerHTML = `
-      <ion-item slot="header" color="light">
-        <ion-label>${capitalizeFirstLetter(pizzaName)}</ion-label>
-      </ion-item>
+    if (isValid(objectName) && isValid(objectDescription)) {
+      const objectTag = document.createElement("ion-item");
+      objectTag.className = "object";
+      objectTag.innerHTML = `
+        <ion-label>${capitalizeFirstLetter(
+          objectName
+        )} (${objectDescription})</ion-label>
 
-      <div class="ion-padding" slot="content">
-        (${formatIngredients(ingredients)})
-      </div>
       `;
 
-      pizzasTag.insertBefore(pizzaTag, pizzasTag.lastChild);
+      objectsTag.insertBefore(objectTag, objectsTag.lastChild);
 
-      formAddPizza.reset();
+      formAddObject.reset();
     } else {
       errorTag.innerHTML = "<p>Formulaire invalide !</p>";
       setTimeout(() => (errorTag.innerHTML = ""), 2000);
@@ -35,9 +31,6 @@ function onDeviceReady() {
 
   function capitalizeFirstLetter(value) {
     return value.trim().slice(0, 1).toUpperCase() + value.trim().slice(1);
-  }
-  function formatIngredients(value) {
-    return value.trim().replaceAll(" ", ", ");
   }
   function isValid(value) {
     return value && value.length > 2 && value.length < 50;
